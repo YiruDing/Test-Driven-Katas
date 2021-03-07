8e2b6d78fc1e49b0a137065eed4a5ed4ab8906c7
Yi-Ru Ding <dingyr0925@gmail.com>
2021-03-06 19:33:52 -0500

    need to work on wrap.spec.js

---
+++ wrap.spec.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai');
+const wrap = require('./wrap');
+
+describe('wrap', () => {
+  it('Returns empty string if empty string was provided', () => {
+    expect(wrap("", 10)).to.equal("");
+  });
+});
