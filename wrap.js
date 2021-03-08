193834a1af1885a7098adec6e97b1f93baa5cfe8
Yi-Ru Ding <dingyr0925@gmail.com>
2021-03-08 13:17:28 -0500

    0308II

--- wrap.js
+++ wrap.js
@@ -1,24 +1,24 @@
-// const wrap = (str, colNum) => {
-//   const words = str.split(" ");
-//   returnStr = "";
-//   lineCount = 0;
+const wrap = (str, colNum) => {
+  const words = str.split(" ");
+  returnStr = "";
+  lineCount = 0;
 
-//   for (let i = 0; i < words.length; i++) {
-//     lineCount += words[i].length;
-//     if (lineCount <= colNum) {
-//       returnStr = returnStr + words[i] + " ";
-//       lineCount += 1;
-//     } else {
-//       returnStr = returnStr + "\n" + words[i] + " ";
-//       lineCount = words[i].length + 1;
-//     }
-//   }
-//   return returnStr;
-// };
+  for (let i = 0; i < words.length; i++) {
+    lineCount += words[i].length;
+    if (lineCount <= colNum) {
+      returnStr = returnStr + words[i] + " ";
+      lineCount += 1;
+    } else {
+      returnStr = returnStr + "\n" + words[i] + " ";
+      lineCount = words[i].length + 1;
+    }
+  }
+  return returnStr;
+};
 
-// console.log(
-//   wrap(
-//     "Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.",
-//     20
-//   )
-// );
+console.log(
+  wrap(
+    "Lorem ipsum dolor sit eu amet, elit na magna sem amet nulla vel purus ac ligula.",
+    20
+  )
+);
